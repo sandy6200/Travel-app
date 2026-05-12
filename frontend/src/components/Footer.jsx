6c4e98c465fe6e0086c0f1688a78620aa111ac70
sandy6200 <sandeepkumar7070263649@gmail.com>
2026-05-12 12:26:41 +0530

    footer fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,32 +2,26 @@ import "./Footer.css";
 import Contactlist from "./Contactlist.jsx";
 
 function Footer() {
-
   const phone = 916200125629;
 
   const generateMessage = (service = "cab booking") => {
     return encodeURIComponent(
-      `Hello, I want to book a ${service}. Please share details.`
+      `Hello, I want to book a ${service}. Please share details.`,
     );
   };
 
   return (
-    <div className="footer mt-5">
-
+    <div className="footer mt-5 footerBox">
       <div className="heading text-center pt-5">
         <h1>Contact Us</h1>
         <p>Ready to book your ride? Get in touch with us!</p>
       </div>
 
-      <div className="container">
-
+      <div className="container footerBox">
         <div className="card custom-card mt-4">
-
           <div className="row w-100">
-
             {/* Left Side */}
             <div className="col-12 col-lg-5 first mt-4">
-
               <Contactlist
                 icon="fa-solid fa-user"
                 name="Owner"
@@ -48,18 +42,13 @@ function Footer() {
                 value="Indore, MP"
                 color="white"
               />
-
             </div>
 
             <div className="col-lg-1"></div>
 
             {/* Right Side */}
             <div className="col-12 col-lg-6 second">
-
-              <a
-                href={`tel:${phone}`}
-                className="btn btn-warning mt-3"
-              >
+              <a href={`tel:${phone}`} className="btn btn-warning mt-3">
                 Call Now
               </a>
 
@@ -71,29 +60,20 @@ function Footer() {
               >
                 Whatsapp
               </a>
-
             </div>
-
           </div>
-
         </div>
-
       </div>
 
-      <p className="allday mt-5 mb-5">
-        Available 24/7 • All Days of the Week
-      </p>
+      <p className="allday mt-5 mb-5">Available 24/7 • All Days of the Week</p>
 
       <div className="copyright pt-5 pb-5">
-        <div className="content1">
-          SANDY TRAVELS - INDORE
-        </div>
+        <div className="content1">SANDY TRAVELS - INDORE</div>
 
         <div className="content2">
           © 2026 Sandy Travels. All rights reserved.
         </div>
       </div>
-
     </div>
   );
 }
